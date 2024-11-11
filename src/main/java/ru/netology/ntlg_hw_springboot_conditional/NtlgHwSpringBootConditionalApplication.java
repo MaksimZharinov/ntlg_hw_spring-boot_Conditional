9023d4802fb429721e495069f69b29371ddb7f93
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-11-12 00:57:37 +0700

    init commit

---
+++ src/main/java/ru/netology/ntlg_hw_springboot_conditional/NtlgHwSpringBootConditionalApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.ntlg_hw_springboot_conditional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtlgHwSpringBootConditionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NtlgHwSpringBootConditionalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/netology/ntlg_hw_springboot_conditional/NtlgHwSpringBootConditionalApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.ntlg_hw_springboot_conditional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtlgHwSpringBootConditionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
